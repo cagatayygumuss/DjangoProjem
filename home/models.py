60498a0796e6c9bb5d8949eb73cfb05060a89545
ogulcanozturkk <ogulcanozturkk@hotmail.com>
2020-04-14 10:23:56 +0300

    hakkimizda.html giris

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Setting(models.Model):
     STATUS = (
@@ -19,9 +20,9 @@ class Setting(models.Model):
     smtpport = models.CharField(blank=True,max_length=5)
     icon = models.ImageField(blank=True,upload_to='images/')
     facebook = models.CharField(blank=True,max_length=50)
-    aboutus = models.TextField()
-    contact = models.TextField()
-    referances = models.TextField()
+    aboutus = RichTextUploadingField(blank=True)
+    contact = RichTextUploadingField(blank=True)
+    referances = RichTextUploadingField(blank=True)
     status = models.CharField(  max_length=10,choices=STATUS)
     create_at = models.DateTimeField (auto_now_add=True)
     update_at = models.DateTimeField (auto_now=True)
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from home import views
+
 urlpatterns = [
     path('', include ('home.urls')),
+    path('hakkimizda/', views.hakkimizda, name='hakkimizda'),
     path('home/', include ('home.urls')),
     path('product/', include ('product.urls')),
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG: # new
-   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
