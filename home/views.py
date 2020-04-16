1726670c9ad02ccc19d2ac6b63a79f96baab866a
ogulcanozturkk <ogulcanozturkk@hotmail.com>
2020-04-16 12:58:50 +0300

    referanslar.html, iletisim.html giris

--- home/views.py
+++ home/views.py
@@ -14,3 +14,13 @@ def hakkimizda(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting, 'page':'hakkimizda'}
     return render(request,'hakkimizda.html',context)
+
+def referanslar(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page':'hakkimizda'}
+    return render(request,'referanslar.html',context)
+
+def iletisim(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page':'hakkimizda'}
+    return render(request,'iletisim.html',context)
