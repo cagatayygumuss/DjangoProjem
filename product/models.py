12fa7023fd5dc0a5189f155683164c6e55a1d23a
ogulcanozturkk <ogulcanozturkk@hotmail.com>
2020-05-04 18:38:25 +0300

    categories updated

--- product/models.py
+++ product/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
+from mptt.models import TreeForeignKey, MPTTModel
 
-class Category(models.Model):
+class Category(MPTTModel):
    STATUS = (
       ('True', 'Evet'),
       ('False', 'Hayir'),
@@ -13,16 +14,28 @@ class Category(models.Model):
    image = models.ImageField(blank=True, upload_to='images/')
    status = models.CharField(max_length=10,choices=STATUS)
    slug = models.SlugField()
-   parent = models.ForeignKey('self',blank=True,null=True,related_name='children',on_delete=models.CASCADE)
+   parent = TreeForeignKey('self',blank=True, null=True ,related_name='children',on_delete=models.CASCADE)
    create_at = models.DateTimeField (auto_now_add=True)
    update_at = models.DateTimeField (auto_now=True)
+
+   class MPTTMeta:
+      order_insertion_by = ['title']
+
+   def __str__(self):
+      return self.title
+
+   def _str_(self):
+      full_path = [self.title]
+      k = self.parent
+      while k is not None:
+         full_path.append(k.title)
+         k= k.parent
+      return '>>' .join(full_path[::-1])
+
 
    def image_tag(self):
       return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
    image_tag.short_description = 'Image'
-
-   def _str_(self):
-      return self.title
 
 
 
