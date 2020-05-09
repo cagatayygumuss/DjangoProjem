c86a532ea2781fd3ea82404f79f81458877842b9
ogulcanozturkk <ogulcanozturkk@hotmail.com>
2020-05-09 12:20:08 +0300

    urun ve kategori iliskileri tamamlandi

--- home/models.py
+++ home/models.py
@@ -15,7 +15,7 @@ class Setting(models.Model):
     address = models.CharField(blank=True,max_length=150)
     phone = models.CharField(blank=True,max_length=15)
     fax = models.CharField(blank=True,max_length=15)
-    email = models.CharField(max_length=50)
+    email = models.CharField(blank=True,max_length=50)
     smtpserver = models.CharField(blank=True,max_length=20)
     smtpemail = models.CharField(blank=True,max_length=20)
     smtppassword = models.CharField(blank=True,max_length=10)
@@ -25,7 +25,7 @@ class Setting(models.Model):
     aboutus = RichTextUploadingField(blank=True)
     contact = RichTextUploadingField(blank=True)
     referances = RichTextUploadingField(blank=True)
-    status = models.CharField(  max_length=10,choices=STATUS)
+    status = models.CharField(max_length=10,choices=STATUS)
     create_at = models.DateTimeField (auto_now_add=True)
     update_at = models.DateTimeField (auto_now=True)
 
--- home/views.py
+++ home/views.py
@@ -4,36 +4,45 @@ from django.shortcuts import render
 
 # Create your views here.
 from home.models import Setting, ContactFormMessage, ContactFormu
-from product.models import Product, Category
+from product.models import Category, Product, Images
 
 
-def index (request):
+def index(request):
     setting = Setting.objects.get(pk=1)
-    sliderdata=Product.objects.all() [:4]
-    category=Category.objects.all()
-
+    sliderdata = Product.objects.all()[:4]
+    category = Category.objects.all()
+    dayproducts = Product.objects.all()[:4]
+    lastproducts = Product.objects.all().order_by('-id')[:4]
+    randomproducts = Product.objects.all().order_by('?')[:4]
 
     context = {'setting': setting,
                'category': category,
-               'page':'home',
-               'sliderdata':sliderdata}
-    return render(request,'index.html',context)
+               'page': 'home',
+               'sliderdata': sliderdata,
+               'dayproducts': dayproducts,
+               'lastproducts': lastproducts,
+               'randomproducts': randomproducts
+               }
+    return render(request, 'index.html', context)
+
 
 def hakkimizda(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting}
-    return render(request,'hakkimizda.html',context)
+    return render(request, 'hakkimizda.html', context)
+
 
 def referanslar(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting}
-    return render(request,'referanslar.html',context)
+    return render(request, 'referanslar.html', context)
+
 
 def iletisim(request):
     if request.method == 'POST':
         form = ContactFormu(request.POST)
         if form.is_valid():
-            data= ContactFormMessage()
+            data = ContactFormMessage()
             data.name = form.cleaned_data['name']
             data.email = form.cleaned_data['email']
             data.subject = form.cleaned_data['subject']
@@ -41,10 +50,31 @@ def iletisim(request):
             data.ip = request.META.get('REMOTE_ADDR')
             data.save()
             messages.success(request, "Mesajiniz basariyla gonderildi.")
-            return HttpResponseRedirect ('/iletisim')
-
+            return HttpResponseRedirect('/iletisim')
 
     setting = Setting.objects.get(pk=1)
     form = ContactFormu()
-    context = {'setting':setting,'form':form}
-    return render(request,'iletisim.html',context)
+    context = {'setting': setting, 'form': form}
+    return render(request, 'iletisim.html', context)
+
+
+def category_products(request, id, slug):
+    category = Category.objects.all()
+    categorydata = Category.objects.get(pk=id)
+    products = Product.objects.filter(category_id=id)
+    context = {'products': products,
+               'category': category,
+               'categorydata': categorydata
+               }
+    return render(request, 'products.html', context)
+
+def product_detail(request,id,slug):
+    category = Category.objects.all()
+    product = Product.objects.get(pk=id)
+    images = Images.objects.filter(product_id=id)
+    context = {'product': product,
+               'category': category,
+               'images': images,
+
+               }
+    return render(request, 'product_detail.html',context)
